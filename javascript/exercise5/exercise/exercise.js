47604f2830ff6d448175d295e73f359432ad9ae5
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-08-03 17:28:52 +0200

    exercise 5 ok

--- javascript/exercise5/exercise/exercise.js
+++ javascript/exercise5/exercise/exercise.js
@@ -7,9 +7,8 @@ const person1 = {
 const person2 = person1;
 
 person2.firstName = "Simon";
-// Modify the property `firstName` of the `person2` in "Simon".
-
 console.log(person1);
 console.log(person2);
 
-//
+// that's because the objects, as the functions, follow the pass by reference property. In that property, the change of a value, that equals another one, 
+//affects the original one, beacause despite the primitive values, the object doesn't make a copy, but acts directly on the originals.
