618823ab977e90fa674cfa200ac2511ea48257dd
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-08-03 13:27:10 +0200

    exercise 9 done

---
+++ javascript/exercise9/es-23/exercise.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+let output = Object.entries(person);
+console.log(output);
+// Print values of person using Object.keys
