d870a7f52b2a38b07adfeda68285d9d7a666659f
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-08-04 10:39:23 +0200

    exercise 10 ok

--- javascript/exercise10/exercise.js
+++ javascript/exercise10/exercise.js
@@ -1,8 +1,18 @@
 //# Optional Chaining In this example we are trying to check if a value for the `city` property is defined. However, the code can be simplified. How can we do that?
 
+const order = {
+  customer: {
+    address: {
+  city:"Palermo"
+    },
+  },
+};
 
-const order = {};
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
-  console.log('City is required');
-}
+
+let doescityexist = order.customer.address?.city;
+if(!doescityexist){
+  console.log ("City is required!");}
+  else{
+    console.log("Registered Successfully!")
+  }
