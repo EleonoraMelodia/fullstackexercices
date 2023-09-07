e672feff8c23eb16e90964c00bffc3f01e68c70e
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-09-07 15:54:10 +0200

    exercise 36 ok

--- javascript/exercise37/exercise.js
+++ javascript/exercise37/exercise.js
@@ -1,15 +1,11 @@
-const number = 1;
-
-function myPromise () {
-return new Promise ((resolve, reject) => { 
-  if(number > 10) {
-    resolve ("The number you wrote is admitted!")
+let number = 1;
+let myPromise = new Promise((resolve, reject) => {
+  if (number > 10) {
+    resolve(`number ${number} is greather than 10!`);
   }
-  else {
-    reject ("sorry, number not admitted!")
-  }
-} )};
+  reject(`'${number}' isn't greater than 10, or meaby not a number`);
+});
 
-myPromise()
-.then(result => console.log(result))
-.catch((err) => console.error(err));
+myPromise
+  .then((result) => console.log(result))
+  .catch((err) => console.error(err));
