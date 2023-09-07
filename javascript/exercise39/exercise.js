f6def38cb89ed89f551fc5b4fad87b09c5cd8a8c
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-09-07 16:16:05 +0200

    exercise 39 ok

--- javascript/exercise39/exercise.js
+++ javascript/exercise39/exercise.js
@@ -1,28 +1,21 @@
-
-//Starting from the previous exercise, try to better handle all errors through the `Error` class and the `catch` method. Also add the `finally` method.
-
-const isLogged = true;
-
-let firstPromise = new Promise((resolve, reject) => {
+let isLogged = true;
+let promiseOne = new Promise((resolve, reject) => {
   if (isLogged) {
     resolve(Math.random());
-  } else {
-    reject(new Error("Sorry, the login wasn't successfully!"));
   }
+  reject(new Error("an error about the login occurred!"));
 });
 
-let secondPromise = (num) => {
-  return new Promise((resolve, reject) => {
-    if (num >= 0.5) {
+let promiseTwo = (number) =>
+  new Promise((resolve, reject) => {
+    if (number > 0.5) {
       resolve({ name: "John", age: 24 });
-    } else {
-      reject(new Error("sorry, this is not a number!"));
     }
+    reject(new Error("sorry, the number is not great enough"));
   });
-};
 
-firstPromise
-.then(secondPromise)
-.then((result)=> console.log(result))
-.catch((err) => console.error(err))
-.finally(() => console.log("always printed, for better or for worse"))
+promiseOne
+  .then(promiseTwo)
+  .then((result) => console.log(result))
+  .catch((err) => console.error(err))
+  .finally(() => console.log(`I'm always printed out`));
